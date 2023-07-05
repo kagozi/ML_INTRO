56081a6478fa1f3127afcfec59b18bf757277670
kagozi <alexkagozi.com>
2023-07-05 22:32:04 +0300

    Completed Regression Lesson

--- mini_project/ud120-projects/datasets_questions/explore_enron_data.py
+++ mini_project/ud120-projects/datasets_questions/explore_enron_data.py
@@ -16,6 +16,83 @@
 """
 
 import joblib
+import pandas as pd
 
 enron_data = joblib.load(open("../final_project/final_project_dataset.pkl", "rb"))
+enron_names_file = "../final_project/poi_names.txt"
 
+# print(enron_data.items())
+# print(len(enron_data.items()))
+count = 0
+
+for person_name, person_data in enron_data.items():
+    if person_data["poi"] == 1:
+        count += 1
+
+with open(enron_names_file, 'r') as file:
+    next(file)
+    
+    poi = 0
+    poi_count = sum(1 for line in file)
+    # print(line for line in file)
+# print("Total number of POIs:", poi_count)
+
+## STOCK BELONGING TO James Prentice
+enron_data["PRENTICE JAMES"]["total_stock_value"]
+
+## Emails from Wesley Colwell to PoI
+enron_data["COLWELL WESLEY"]["from_this_person_to_poi"]
+
+## What’s the value of stock options exercised by Jeffrey K Skilling?
+# print(enron_data["SKILLING JEFFREY K"]["exercised_stock_options"])
+
+## Executive with most money
+executives = [
+    "SKILLING JEFFREY K",
+    "FASTOW ANDREW S",
+    "LAY KENNETH L"
+]
+for executive in executives:
+    if executive in enron_data:
+        total_payments = enron_data[executive]["total_payments"]
+        # print(f"Total payments for {executive}: {total_payments}")
+    else:
+        print(f"No data available for {executive}")
+
+
+salary_count = 0
+email_count = 0
+
+for person_data in enron_data.values():
+    if person_data.get('salary'):
+        salary_count += 1
+    if person_data.get('email_address'):
+        email_count += 1
+# print(enron_data.keys())
+# print("Number of folks with a quantified salary:", salary_count)
+# print("Number of folks with a known email address:", email_count)
+total_people = len(enron_data)
+nan_payments_count = 0
+
+for person_data in enron_data.values():
+    if person_data.get('total_payments') == 'NaN':
+        nan_payments_count += 1
+
+percentage_nan_payments = (nan_payments_count / total_people) * 100
+
+print("Number of people with 'NaN' for total payments:", nan_payments_count)
+print("Percentage of people with 'NaN' for total payments:", percentage_nan_payments)
+
+poi_count = 0
+poi_nan_payments_count = 0
+
+for person_data in enron_data.values():
+    if person_data.get('poi'):
+        poi_count += 1
+        if person_data.get('total_payments') == 'NaN':
+            poi_nan_payments_count += 1
+
+percentage_poi_nan_payments = (poi_nan_payments_count / poi_count) * 100
+print(poi_count)
+# print("Number of POIs with 'NaN' for total payments:", poi_nan_payments_count)
+# print("Percentage of POIs with 'NaN' for total payments:", percentage_poi_nan_payments)
--- mini_project/ud120-projects/regression/finance_regression.py
+++ mini_project/ud120-projects/regression/finance_regression.py
@@ -15,6 +15,7 @@
 import os
 import sys
 import joblib
+from sklearn import linear_model
 sys.path.append(os.path.abspath("../tools/"))
 from feature_format import featureFormat, targetFeatureSplit
 dictionary = joblib.load( open("../final_project/final_project_dataset_modified.pkl", "rb") )
@@ -30,7 +31,7 @@ target, features = targetFeatureSplit( data )
 from sklearn.model_selection import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -38,12 +39,32 @@ test_color = "b"
 ### Please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
+reg =linear_model.LinearRegression()
+reg.fit(feature_train,target_train)
+print(reg.coef_)
+print(reg.intercept_)
+score = reg.score(feature_train, target_train)
+print("R^2 score on training data:", score)
+score_test = reg.score(feature_test, target_test)
+print("R^2 score on testing data:", score_test)
 
 
+## Bonus against longterm incentive
+# Regress bonus against long term incentive
+# features_list = ["bonus", "long_term_incentive"]
+# data = featureFormat(dictionary, features_list, remove_any_zeroes=True, sort_keys='../tools/python2_lesson06_keys.pkl')
+# target, features = targetFeatureSplit(data)
 
+# # Split the data into training and testing sets
+# feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 
+# # Create and fit the regression model
+# reg = linear_model.LinearRegression()
+# reg.fit(feature_train, target_train)
 
-
+# # Calculate the score on the test data
+# score_test_long_term_incentive = reg.score(feature_test, target_test)
+# print("R^2 score for bonus against long term incentive (test data):", score_test_long_term_incentive)
 
 
 ### draw the scatterplot, with color-coded training and testing points
@@ -66,6 +87,13 @@ try:
 except NameError:
     pass
 plt.xlabel(features_list[1])
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="b")
+
+# Calculate the slope of the new regression line
+slope = reg.coef_[0]
+print("Slope of the new regression line:", slope)
+
 plt.ylabel(features_list[0])
 plt.legend()
 plt.show()
