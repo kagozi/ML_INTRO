aaa5b484b611ebaaaa4ed7f1ef30272f44527a9f
kagozi <alexkagozi.com>
2023-07-12 14:18:45 +0300

    Feature Selection Mini-project

--- mini_project/ud120-projects/feature_selection/find_signature.py
+++ mini_project/ud120-projects/feature_selection/find_signature.py
@@ -3,15 +3,16 @@
 import joblib
 import numpy
 numpy.random.seed(42)
-
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import metrics 
 
 ### The words (features) and authors (labels), already largely processed.
 ### These files should have been created from the previous (Lesson 10)
 ### mini-project.
-words_file = "../text_learning/your_word_data.pkl" 
-authors_file = "../text_learning/your_email_authors.pkl"
-word_data = joblib.load( open(words_file, "r"))
-authors = joblib.load( open(authors_file, "r") )
+words_file = "./your_word_data.pkl"
+authors_file = "./your_email_authors.pkl" 
+word_data = joblib.load( open(words_file, "rb"))
+authors = joblib.load( open(authors_file, "rb") )
 
 
 
@@ -20,13 +21,25 @@ authors = joblib.load( open(authors_file, "r") )
 ### feature matrices changed to dense representations for compatibility with
 ### classifier functions in versions 0.15.2 and earlier
 from sklearn.model_selection import train_test_split
+print(type(word_data))
+print(type(authors))
+print(len(word_data))
+print(len(authors))
+
 features_train, features_test, labels_train, labels_test = train_test_split(word_data, authors, test_size=0.1, random_state=42)
+
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5, stop_words='english')
 features_train = vectorizer.fit_transform(features_train)
 features_test  = vectorizer.transform(features_test).toarray()
+# Get the feature names (associated words)
+feature_names = vectorizer.get_feature_names_out()
 
+# ...
+
+# Print all the associated words
+print(feature_names)
 
 ### a classic way to overfit is to use a small number
 ### of data points and a large number of features;
@@ -37,6 +50,25 @@ labels_train   = labels_train[:150]
 
 
 ### your code goes here
+num_features = len(features_train[0])
+# print(num_features)
+# print(len(labels_train))
+clf = DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train, labels_train)
+feature_importances = clf.feature_importances_
 
+imp = numpy.max(feature_importances)
+print(imp)
+index_of_maximum = numpy.where(feature_importances == imp)
+print("Maximum Index position: ",index_of_maximum)
+# Find the index of the most important feature
+max_importance_index = numpy.argmax(feature_importances)
 
+# Get the associated word
+most_powerful_word = feature_names[max_importance_index]
 
+# Print the most powerful word
+print("Most Powerful Word:", most_powerful_word)
+labels_pred = clf.predict(features_test)
+accuracy = metrics.accuracy_score(labels_pred,labels_test)
+print(accuracy)
--- mini_project/ud120-projects/text_learning/vectorize_text.py
+++ mini_project/ud120-projects/text_learning/vectorize_text.py
@@ -44,24 +44,27 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
         ### once everything is working, remove this line to run over full dataset
         temp_counter += 1
         if temp_counter < 200:
-	        path = os.path.join('..', path[:-1])
-	        print(path)
-	        email = open(path, "r")
+            path = os.path.join('..', path[:-1])
+            print(path)
+            email = open(path, "r")
 
 	        ### use parseOutText to extract the text from the opened email
-
-
+            text = parseOutText(email)
 	        ### use str.replace() to remove any instances of the words
 	        ### ["sara", "shackleton", "chris", "germani"]
-
-
+            text = text.replace("sara", "")
+            text = text.replace("shackleton", "")
+            text = text.replace("chris", "")
+            text = text.replace("germani", "")
 	        ### append the text to word_data
-
+            word_data.append(text)
 
 	        ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
-
-	        email.close()
+            if name == "sara":
+                from_data.append(0)
+            else:
+                from_data.append(1)
+            email.close()
 
 print("Emails Processed")
 from_sara.close()
